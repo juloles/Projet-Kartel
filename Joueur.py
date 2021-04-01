f4870c701224b2bc62273d68d5c535234e792d5b
juloles <79644337+juloles@users.noreply.github.com>
2021-04-01 14:08:48 +0200

    changement dé et joueur

--- Joueur.py
+++ Joueur.py
@@ -1,7 +1,19 @@
 ### Joueur ###
 from Inspecteur import*
-from Dé import*
+from De import*
 
 class Joueur :
 
-    def __init__(self,):
+    def __init__(self):
+        self.points=0
+        self.de=De()
+
+
+
+    def mouvement(self):
+
+        for i in range (self.de.resultats_de()):
+            print (self.de.resultats_de())
+            nb_cases=int(input("de combien de cases voulez vous avancer ?"))
+            if nb_cases>self.de.resultats_de():
+                print ("veuillez choisir un autre plus petit que le résultat du dé")
