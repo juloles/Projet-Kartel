fd8c2f2676702d4ddf14ea36453aab1a1495a4c5
juloles <79644337+juloles@users.noreply.github.com>
2021-04-01 13:51:06 +0200

    Jeton

--- Jeton.py
+++ Jeton.py
@@ -1,1 +1,10 @@
 ### Jeton ###
+from Argent import *
+from Boss import *
+from Gangster import *
+
+class Jeton:
+    def __init__(self):
+        self.couleur=None
+        self.etat=True ### True à l'état libre, False pour l'état capturé
+        
