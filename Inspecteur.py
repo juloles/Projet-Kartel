da6d548ce8b5dcf89d475659da48995d34bb08fc
AlexVautrin <alexandre.vautrin21560@gmail.com>
2021-04-01 14:59:23 +0200

    Inspector

--- Inspecteur.py
+++ Inspecteur.py
@@ -1,10 +1,11 @@
 ### Inspecteur ###
 from Case import *
 from Joueur import *
+from Plateau import *
 
 class Inspecteur :
 
     def __init__ (self) :
         None
 
-    def deplacement (self) :
+    def deplacement (self , nb_cases) :
