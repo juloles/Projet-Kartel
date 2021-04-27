bcf43dbf8ef4aeb2568baa9d0522c531623958a3
juloles <79644337+juloles@users.noreply.github.com>
2021-04-27 22:05:26 +0200

    tab et case

--- Case.py
+++ Case.py
@@ -5,7 +5,35 @@ from Gangster import *
 
 class Case:
     def __init__(self):
+
         self.case=[" "]
 
+
     def affichage(self):
+
         print (self.case, end=' ')
+
+    def remplir_case(self, personnage, couleur):
+
+        if personnage=="Argent":
+
+            self.case=["€"]
+
+        if personnage=="Boss":
+
+            self.case=["B"]
+
+        if personnage=="1Gangster":
+
+            self.case=["1G"]
+
+        if personnage=="2Gangster":
+
+            self.case=["2G"]
+
+        if personnage=="3Gangster":
+
+            self.case=["3G"]
+
+
+
--- Plateau.py
+++ Plateau.py
@@ -5,22 +5,51 @@ from Prison import*
 class Plateau():
 
     def __init__(self,cases):
+
         self.nombre_cases=cases
-        self.plateau=[0]*self.nombre_cases
+
+        self.plateau=[' ']*self.nombre_cases
+
         for i in range (self.nombre_cases):
+
             self.plateau[i]=Case()
 
 
+
+
     def afficher(self):
+
         print("Plateau :", end=' ')
+
         for i in range(self.nombre_cases):
 
             self.plateau[i].affichage()
 
+
+
 ## remplir le plateau de manière aléatoire
+
+    def remplir_plateau(self):
+
+        tableau_personnages=["Argent", "1Gangster", "2Gangster", "2Gangster", "3Gangster", "Boss"]
+
+        tableau_couleur=["R", "Ve", "J", "B", "V", "M", "Ro"]
+
+
+        for i in range (len(tableau_personnages)):
+
+            for j in range (len(tableau_couleur)):
+
+                for r in range(self.nombre_cases):
+                    self.plateau[r]=self.plateau[r].remplir_case(i,j)
+
+        print (self.plateau[r])
+
+
 
 
 
 
 P=Plateau(43)
 P.afficher()
+P.remplir_plateau()
