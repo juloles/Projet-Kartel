3cb4e4aca049a364a676e014f2af590de911bfbb
AlexVautrin <alexandre.vautrin21560@gmail.com>
2021-04-27 22:54:39 +0200

    Update Plateau.py

--- Plateau.py
+++ Plateau.py
@@ -9,11 +9,8 @@ class Plateau():
     def __init__(self,cases):
 
         self.nombre_cases=cases
-
         self.plateau=[' ']*self.nombre_cases
-
         for i in range (self.nombre_cases):
-
             self.plateau[i]=Case()
 
 
@@ -22,9 +19,7 @@ class Plateau():
     def afficher(self):
 
         print("Plateau :", end=' ')
-
         for i in range(self.nombre_cases):
-
             self.plateau[i].affichage()
 
 
@@ -34,21 +29,25 @@ class Plateau():
     def remplir_plateau(self):
 
         tableau_personnages=["Argent", "1Gangster", "2Gangster", "2Gangster", "3Gangster", "Boss"]
-
         tableau_couleur=["Rouge", "Vert", "Jaune", "Bleu", "Violet", "Marron", "Rose"]
 
 
         for i in range (len(tableau_personnages)):
-
             for j in range (len(tableau_couleur)):
-
                     self.plateau[i*7+j].remplir_case(tableau_personnages [i],tableau_couleur[j])
+        print("\n")
         print("\nPlateau rempli :")
         self.afficher()
 
     def random_plateau(self):
 
-        random.shuffle(self.plateau)
+        c = Case()
+        for i in range (0,10000) :
+            r1 = randint(0,42)
+            r2 = randint(0,42)
+            c = self.plateau[r1]
+            self.plateau[r1] = self.plateau[r2]
+            self.plateau[r2] = c
 
         print("\n")
         print("\nPlateau rempli random :")
